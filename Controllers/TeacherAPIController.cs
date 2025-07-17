5a9f3330c68296ec6b38a196337dcdc511b76718
Samson Mathews <samsonmmathews@gmail.com>
2025-07-17 14:10:07 -0400

    Teacher api controller code in progress

--- Controllers/TeacherAPIController.cs
+++ Controllers/TeacherAPIController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Backend_Cumulative_01.Controllers
 {
@@ -7,5 +9,36 @@ namespace Backend_Cumulative_01.Controllers
     [ApiController]
     public class TeacherAPIController : ControllerBase
     {
+        /// <summary>
+        /// This method should connect to the database and extract the teachers' names
+        /// </summary>
+        /// <returns>
+        /// A list of teachers' names
+        /// </returns>
+        /// <example>
+        /// 
+        /// </example>
+        [HttpGet(template: "ListTeacherNames")]
+        public List<string> ListTeacherNames()
+        {
+            // Creates an empty list of teacher names
+            List<string> TeacherNames = new List<string>();
+
+            // Open the connection to the database
+            MySqlConnection Connection =
+
+            // Write a query - "select * from schooldb"
+
+            // "run" the query against the database
+
+            // get the response from the database as a "Result Set"
+
+            // loop through the results one at a time
+
+            // add the teachers one at a time
+
+            //return the teachers' names
+            return TeacherNames;
+        }
     }
 }
