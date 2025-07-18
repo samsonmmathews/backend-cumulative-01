f5b1e4d5b1247f041009c64deebe28c08d70c65c
Samson Mathews <samsonmmathews@gmail.com>
2025-07-18 15:14:57 -0400

    Added comments

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -2,11 +2,17 @@
 {
     public class Teacher
     {
+        // A Teacher ID primary key 
         public int TeacherId { get; set; }
+        // the first name of the teacher
         public string TeacherFname { get; set; }
+        // the last name of the teacher
         public string TeacherLname { get; set; }
+        // the employee number of the teacher
         public string EmployeeNumber { get; set; }
+        // the hire date of the teacher
         public DateTime HireDate { get; set; }
+        // the salary of the teacher
         public decimal Salary { get; set; }
     }
 }
