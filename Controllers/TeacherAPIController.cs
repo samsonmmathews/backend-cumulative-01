3591445cc22c48e2372a71fad240a3fac148f299
Samson Mathews <samsonmmathews@gmail.com>
2025-07-17 20:09:58 -0400

    First question done

--- Controllers/TeacherAPIController.cs
+++ Controllers/TeacherAPIController.cs
@@ -2,60 +2,67 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using System.IO.Pipelines;
 using System.Reflection.Metadata.Ecma335;
 
 namespace Backend_Cumulative_01.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Teacher")]
     [ApiController]
     public class TeacherAPIController : ControllerBase
     {
         // get access to the blog object
-        private SchoolDbContext School = new SchoolDbContext();
+        private SchoolDbContext _context = new SchoolDbContext();
 
         /// <summary>
-        /// This method should connect to the database and extract the teachers' names
+        /// This method should connect to the database and extract the details of all teachers
         /// </summary>
         /// <returns>
-        /// A list of teachers' names
+        /// Information on all teachers
         /// </returns>
         /// <example>
         /// 
         /// </example>
-        [HttpGet(template: "ListTeacherNames")]
-        public List<string> ListTeacherNames()
+        [HttpGet(template: "ListTeachers")]
+        public List<Teacher> ListTeachers()
         {
             // Creates an empty list of teacher names
-            List<string> TeacherNames = new List<string>();
+            List<Teacher> TeacherNames = new List<Teacher>();
 
             // Create the connection to the database
-            MySqlConnection Connection = School.AccessDatabase();
-
-            // Open the connection to the database
-            Connection.Open();
-
-            // Write a query - "select * from schooldb"
-            string query = "select * from teachers";
-
-            // create a command
-            MySqlCommand Command = Connection.CreateCommand();
-
-            // set the command text to the query
-            Command.CommandText = query;
-
-            // run the command against the database
-            // get the response from the database as a "Result Set"
-            MySqlDataReader ResultSet = Command.ExecuteReader();
-
-            // loop through the results one at a time
-            while(ResultSet.Read())
+            using (MySqlConnection Connection = _context.AccessDatabase())
             {
-                // add the first names one at a time
-                string TeacherName = ResultSet["teacherfname"].ToString();
-                TeacherNames.Add(TeacherName);
-            }
 
-            // add the teachers one at a time
+                // Open the connection to the database
+                Connection.Open();
+
+                // Write a query - "select * from schooldb"
+                string query = "select * from teachers";
+
+                // create a command
+                MySqlCommand Command = Connection.CreateCommand();
+
+                // set the command text to the query
+                Command.CommandText = query;
+
+                // run the command against the database
+                // get the response from the database as a "Result Set"
+                using (MySqlDataReader ResultSet = Command.ExecuteReader())
+                {
+                    // loop through the results one at a time
+                    while (ResultSet.Read())
+                    {
+                        Teacher TeacherData = new Teacher();
+                        TeacherData.TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                        TeacherData.TeacherFname = ResultSet["teacherfname"].ToString();
+                        TeacherData.TeacherLname = ResultSet["teacherlname"].ToString();
+                        TeacherData.EmployeeNumber = ResultSet["employeenumber"].ToString();
+                        TeacherData.HireDate = DateTime.Parse(ResultSet["hiredate"].ToString());
+                        TeacherData.Salary = decimal.Parse(ResultSet["salary"].ToString());
+                        TeacherNames.Add(TeacherData);
+                    }
+                }
+            }
 
             //return the teachers' names
             return TeacherNames;
--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -2,5 +2,11 @@
 {
     public class Teacher
     {
+        public int TeacherId { get; set; }
+        public string TeacherFname { get; set; }
+        public string TeacherLname { get; set; }
+        public string EmployeeNumber { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
     }
 }
